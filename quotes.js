590645549e407612bc17d955f06409b8465b848e
namorima <84259399+namorima@users.noreply.github.com>
2021-09-02 09:04:46 +0800

    update

--- quotes.js
+++ quotes.js
@@ -1,7 +1,7 @@
 //Quotes Changed Automatically		 
 $(function () { 
   count = 0; 
-  wordsArray = ["Ilmu tanpa agama adalah suatu kecacatan, dan agama tanpa ilmu merupakan kebutaan", "Kerjakanlah urusan duniamu seakan-akan kamu hidup selamanya. Dan laksanakanlah urusan akhiratmu seakan-akan kamu akan mati besok", "Ketika terbukti salah, orang bijak akan memperbaiki dirinya sendiri dan orang yang bodoh akan terus berdebat", "Kualitas pikiranmu menentukan kualitas kehidupanmu"]; 
+  wordsArray = ["Ilmu tanpa agama adalah suatu kecacatan, dan agama tanpa ilmu merupakan kebutaan", "test2", "Ketika terbukti salah, orang bijak akan memperbaiki dirinya sendiri dan orang yang bodoh akan terus berdebat", "Kualitas pikiranmu menentukan kualitas kehidupanmu"]; 
   setInterval(function () { 
     count++; 
     $("#word").fadeOut(500, function () { 
