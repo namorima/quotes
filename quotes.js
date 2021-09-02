5f67e381fadf737aed2f32ddab7ad4aff1fd91f2
namorima <84259399+namorima@users.noreply.github.com>
2021-09-02 08:24:44 +0800

    added

---
+++ quotes.js
@@ -0,0 +1,12 @@
+//Quotes Changed Automatically		 
+$(function () { 
+  count = 0; 
+  wordsArray = ["Ilmu tanpa agama adalah suatu kecacatan, dan agama tanpa ilmu merupakan kebutaan", "Kerjakanlah urusan duniamu seakan-akan kamu hidup selamanya. Dan laksanakanlah urusan akhiratmu seakan-akan kamu akan mati besok", "Ketika terbukti salah, orang bijak akan memperbaiki dirinya sendiri dan orang yang bodoh akan terus berdebat", "Kualitas pikiranmu menentukan kualitas kehidupanmu"]; 
+  setInterval(function () { 
+    count++; 
+    $("#word").fadeOut(500, function () { 
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(500); 
+    }); 
+  }, 8000); 
+}); 
+//End Replace Text function
